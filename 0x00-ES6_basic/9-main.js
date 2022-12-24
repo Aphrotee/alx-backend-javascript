83760a02370810301479290442459b30824f4fd3
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-12-24 16:31:25 +0100

    completed tasks 5 through 10

---
+++ 0x00-ES6_basic/9-main.js
@@ -0,0 +1,5 @@
+import getFullBudgetObject from './9-getFullBudget';
+
+const fullBudget = getFullBudgetObject(20, 50, 10);
+console.log(fullBudget.getIncomeInDollars(fullBudget.income));
+console.log(fullBudget.getIncomeInEuros(fullBudget.income));
