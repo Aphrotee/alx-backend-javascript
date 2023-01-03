6968000e0b96a8c07b55df73dab09dc2b2c8993e
Aiyebogan Temitope <94745676+Aphrotee@users.noreply.github.com>
2023-01-03 13:29:48 +0000

    task 5 completed

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,5 +1,3 @@
 export default function uploadPhoto(filename) {
-  return (new Promise((reject) => {
-    reject(new Error(`${filename} cannnot be processed`));
-  }));
+  return Promise.reject(new Error(`${filename} cannnot be processed`));
 }
