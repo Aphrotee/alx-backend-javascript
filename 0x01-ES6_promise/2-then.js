f31ec817ac1d4f02ae64d7d812c80831f57fbee4
Aiyebogan Temitope <94745676+Aphrotee@users.noreply.github.com>
2023-01-03 05:34:37 +0000

    tasks 2, 5, 6 done

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
-    return ({ status: 200, body: 'success' });
-  })
-    .catch(() => {
-      return (new Error());
-    }).finally(() => {
-      console.log('Got a response from the API');
-    });
+  promise
+    .then(() => ({ status: '200', body: 'success' }))
+    .catch(() => (new Error()))
+    .finally(() => { console.log('Got a response from the API'); });
 }
