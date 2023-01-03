ef95d83d9747a41366126fdda63b035b8033a0b3
Aiyebogan Temitope <94745676+Aphrotee@users.noreply.github.com>
2023-01-03 13:22:30 +0000

    fixed reeor with semicolon

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,5 +1,5 @@
 export default function uploadPhoto(filename) {
-  return new Promise((reject) => {
+  return (new Promise((reject) => {
     reject(new Error(`${filename} cannnot be processed`));
-  });
+  }));
 }
