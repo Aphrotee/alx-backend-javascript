0bbe97787b8b0e0776696d629dbfdb59496074cb
Aiyebogan Temitope <94745676+Aphrotee@users.noreply.github.com>
2023-01-03 02:48:53 +0000

    task 0 completed and tested

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,4 +1,4 @@
-export default function getResponseFromApi() {
+export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
     const a = 1 + 1;
     if (a) { resolve('Success!'); } else { reject(Error('Failure!')); }
