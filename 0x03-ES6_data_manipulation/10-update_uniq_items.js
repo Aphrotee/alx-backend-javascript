43af7e694ccf5e042bb4c147d320fffbaeebca3b
Aiyebogan Temitope <94745676+Aphrotee@users.noreply.github.com>
2023-01-04 14:47:06 +0000

    tasks 10 & 11

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,10 +1,7 @@
 export default function updateUniqueItems(map) {
-    if (!(map instanceof Map)) {
-      throw new Error('Cannot process');
-    }
-    map.forEach((value, key) => {
-      if (value === 1) {
-        map.set(key, 100);
-      }
-    });
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
   }
+  map.forEach((value, key, currentMap) => { if (value === 1) { currentMap.set(key, 100); } });
+  return map;
+}
