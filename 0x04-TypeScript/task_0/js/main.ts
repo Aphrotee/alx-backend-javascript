c218b603496979c2baac6a8fff96076a7e82cb7b
root <root@LAPTOP-6DSA3SNQ.localdomain>
2023-01-08 15:03:27 +0100

    task )

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,58 @@
+interface Student {
+    firstName:string;
+    lastName:string;
+    age:number;
+    location:string;
+}
+
+
+const student1: Student = {
+    firstName: 'Temitope',
+    lastName: 'Áiyebogan',
+    age: 19,
+    location: 'Ilaje',
+}
+
+const student2: Student = {
+    firstName: 'Ayomide',
+    lastName: 'Ogunnoiki',
+    age: 19,
+    location: 'Ikeja',
+}
+
+const studentArray: Student[] = [student1, student2];
+
+const table = document.createElement('table');
+const thead = document.createElement('thead');
+const tbody = document.createElement('tbody');
+const trH = document.createElement('tr');
+
+const h1 = document.createElement('th');
+const h2 = document.createElement('th');
+
+h1.innerHTML = 'firstName';
+h2.innerHTML = 'location';
+
+trH.appendChild(h1);
+trH.appendChild(h2);
+
+for (let studentInt of studentArray) {
+  let trB = document.createElement('tr');
+  let c1 = document.createElement('td');
+  let c2 = document.createElement('td');
+  c1.innerHTML = studentInt.firstName;
+  c2.innerHTML = studentInt.location;
+  trB.appendChild(c1);
+  trB.appendChild(c2);
+  tbody.appendChild(trB);
+}
+
+  const trB = document.createElement('tr');
+  const c1 = document.createElement('td');
+  const c2 = document.createElement('td');
+  c1.innerHTML = '';
+  c2.innerHTML = '';
+  trB.appendChild(c1);
+  trB.appendChild(c2);
+  tbody.appendChild(trB);
+  document.appendChild(table);
