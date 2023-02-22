8f35a5cee6ea44a5b104ef39bfe4c24d75166668
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2023-02-22 08:25:56 +0100

    in progress

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,10 @@
-function displayMessage(message) {
-  console.log(message);
-}
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
 
 module.exports = displayMessage;
