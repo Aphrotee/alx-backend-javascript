43357b82ed2f5209b7eeff4aea66f0efa6c16a42
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2023-02-24 23:57:06 +0100

    test 0

---
+++ 0x06-unittests_in_JS/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: false,
+    es6: true,
+    jest: false,
+  },
+  extends: [
+    'airbnb-base'  
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'max-classes-per-file': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
+};
